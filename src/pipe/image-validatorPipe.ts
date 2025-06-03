24ed6b9fbbab1202763b631a76c2cb171d27a7be
phap <nguyennhatphapbp@gmail.com>
2025-06-03 12:12:55 +0700

    fix: extend memory to upload

--- src/pipe/image-validatorPipe.ts
+++ src/pipe/image-validatorPipe.ts
@@ -1,7 +1,7 @@
 import { HttpStatus, ParseFilePipeBuilder } from '@nestjs/common';
 
 const imageValidatorPipe = new ParseFilePipeBuilder()
-  .addMaxSizeValidator({ maxSize: 1024 * 1024 * 5 })
+  .addMaxSizeValidator({ maxSize: 1024 * 1024 * 20 })
   .addFileTypeValidator({
     fileType: /^image\/(png|jpg|jpeg|bmp|webp)$/,
   })
